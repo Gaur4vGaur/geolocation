37b35f689140c7b50c1249d4e6584ecbd2b06338
Gaurav Gaur <vaibgaurav@gmail.com>
2017-06-28 22:01:54 +0100

    Added exception handling code

--- version1/script.js
+++ version1/script.js
@@ -1,3 +1,4 @@
+// method to display the geolocation
 function displayLocation(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
@@ -6,9 +7,17 @@ function displayLocation(position) {
     pLocation.innerHTML += latitude + "," + longitude + "<br>";
 }
 
+// Error handling code
+function displayError(error) {
+    var errors = ["Unknown error", "Permissions denied by user", "Position not available", "Timeout error"];
+    var message = error[error.code];
+    console.warn("Error in getting your location" + message + error.message);
+}
+
 window.onload = function() {
     if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(displayLocation);
+        // display location is function passed to getCurrentPosition for callback with position
+        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
     } else {
         alert("Sorry, this browser doesn't support geolocation!");
     }
